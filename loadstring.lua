f6aeb349455ebe43718df8f899ffdae0505d03e9
Vezun148 <kolavoja076@gmail.com>
2025-09-17 22:51:24 +0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,4 +1,4 @@
-local PremiumModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vezun148/k1lly0u-/refs/heads/main/prem.lua"))()
+local PremiumModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/assets/Premium"))()
 
 local function LoadScript(url)
     local success, scriptContent = pcall(game.HttpGet, game, url)
@@ -22,5 +22,11 @@ local function LoadScript(url)
     return true
 end
 
-
-
+if not shared.VertexLoaded then
+    shared.VertexLoaded = true
+    local url = "https://raw.githubusercontent.com/vertex-peak/vertex/main/modules/" .. game.PlaceId .. ".lua"
+    local success = LoadScript(url)
+    if not success then
+        LoadScript("https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/universal")
+    end
+end
